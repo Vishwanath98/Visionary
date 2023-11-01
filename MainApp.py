dbfd7d9e4daacb98880a77504bfbc721bfd6a859
sandeep reddy potula <potulasandeep123@gmail.com>
2023-10-31 20:46:42 -0700

    pycharm

---
+++ MainApp.py
@@ -0,0 +1,7 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import QCoreApplication
+from LoginDialog import *
+from PyQt5 import QtWidgets
+from RegisterDialog import *
+from User_Details import *
