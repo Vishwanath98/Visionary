eaf95cffa2e0905eb3719e65202eed7e387e3721
Sandeep Reddy Potula <potulasandeep123@gmail.com>
2023-11-08 15:03:05 -0800

    patient_home_page added

---
+++ Patient_home_page.py
@@ -0,0 +1,9 @@
+from PyQt5.QtWidgets import QDialog
+from PyQt5.uic import loadUi
+from DATA225utils import make_connection
+from LoginDialog import *
+class Patient_Home(QDialog):
+    def __init__(self):
+        super(Patient_Home,self).__init__()
+        self.ui = uic.loadUi("Patient_home_page.ui",self)
+        self.setWindowTitle("Patient Home Page")
